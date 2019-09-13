63604d10c4e565bd1060ec294893c8cbebaba340
Ryan Mason-Davies <iotize@icloud.com>
2019-09-13 10:06:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import express from 'express';
+const express = require("express");
+const PORT = process.env.PORT || 5000
 const app = express()
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
